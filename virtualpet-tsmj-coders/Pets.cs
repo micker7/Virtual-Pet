5e411c379465834115cef077c921e71f9247a7b9
Michaela (Mickee) Young <122296624+micker7@users.noreply.github.com>
2023-03-31 18:36:04 -0400

    Add files via upload

---
+++ virtualpet-tsmj-coders/Pets.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template_csharp_virtual_pet
+{
+    public abstract class Pets
+    {
+        public static int idNumber = 100;
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int IdNumber { get; set; }
+        //examples of encapsulation^
+        
+        
+        public abstract void Feed();    
+        public abstract void Play();
+        public abstract void Doctor();
+        public abstract void Print();
+        public abstract void Tick();
+        public abstract void Status();
+        //examples of polymorphism^
+    }
+}
